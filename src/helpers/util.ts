9687ea6f5bb983120012e7b039e5e452bf08b448
liangdaqin <liang_dq@hisuntech.com>
2020-02-19 17:59:08 +0800

    feat:拓展接口

--- src/helpers/util.ts
+++ src/helpers/util.ts
@@ -1,3 +1,4 @@
+import { AnyARecord } from "dns"
 
 const toString = Object.prototype.toString
 
@@ -11,4 +12,11 @@ export function isDate(val: any): val is Date {
 
 export function isPlainObject(val: any): val is Object {
   return toString.call(val) === '[object object]'
+}
+
+export function extend<T, U>(to: T, from: U): T & U {
+  for (const key in from) {
+    (to as T & U)[key] = from[key] as any
+  }
+  return to as T & U
 }
